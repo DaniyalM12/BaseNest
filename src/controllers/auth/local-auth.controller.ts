2e8c8cf281a347cecb2f5bce6c3a1955a5d057f3
zbook <daniyal.moeen@express.com.pk>
2022-03-30 14:58:07 -0700

    Code refactor

--- src/controllers/auth/local-auth.controller.ts
+++ src/controllers/auth/local-auth.controller.ts
@@ -1,91 +1,84 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { CreateUserDto } from '../../core/dtos';
-import {
-  LocalAuthFactoryService,
-  LocalAuthService,
-} from '../../services/use-cases/passport/local-auth';
-import { BaseResponseDto } from '../../core/dtos/response/base-response.dto';
-import { User } from '../../core/entities';
-import { Roles } from '../../enums/roles.enums';
+import {Body, Controller, Get, Param, Post, UseGuards} from '@nestjs/common';
+import {AuthGuard} from '@nestjs/passport';
+import {CreateUserDto} from '../../core/dtos';
+import {LocalAuthFactoryService, LocalAuthService,} from '../../services/use-cases/passport/local-auth';
+import {BaseResponseDto} from '../../core/dtos/response/base-response.dto';
+import {User} from '../../core/entities';
+import {Roles} from '../../enums/roles.enums';
 import {ValidationPipe} from "../../pipes";
 
 @Controller('auth')
 export class LocalAuthController {
-  constructor(
-    private localAuthFactoryService: LocalAuthFactoryService,
-    private localAuthService: LocalAuthService,
-  ) {}
-
-  @Post('register/:user')
-  async register(
-    @Param('user') user: string,
-    @Body(new ValidationPipe()) registerUserDto: CreateUserDto,
-  ) {
-    const baseResponseDto: BaseResponseDto<User> = new BaseResponseDto();
-    try {
-      switch (user) {
-        case Roles.DEALER: {
-          const user = await this.localAuthFactoryService.createNewUser(
-            registerUserDto,
-          );
-          const createdUser = await this.localAuthService.registerUser(user,Roles.DEALER);
-          baseResponseDto.successExecWithoutType(user);
-
-          break;
-        }
-        case Roles.MERCHANT: {
-          //merchant will have seperate fields and Dto and validation logic
-          const user = await this.localAuthFactoryService.createNewUser(
-            registerUserDto,
-          );
-          const createdUser = await this.localAuthService.registerUser(user,Roles.MERCHANT);
-          baseResponseDto.successExecWithoutType(user);
-
-          break;
-        }
-        case Roles.SERVICE_PROVIDER: {
-          //SP will have seperate fields and Dto and validation logic
-          const user = await this.localAuthFactoryService.createNewUser(
-            registerUserDto,
-          );
-          const createdUser = await this.localAuthService.registerUser(user,Roles.SERVICE_PROVIDER);
-          baseResponseDto.successExecWithoutType(user);
-          break;
-        }
-        default: {
-          baseResponseDto.errorExec(new Error('Invalid Request'));
-          break;
-        }
-      }
-    } catch (error) {
-      // report and log error
-      baseResponseDto.errorExec(error);
+    constructor(
+        private localAuthFactoryService: LocalAuthFactoryService,
+        private localAuthService: LocalAuthService,
+    ) {
     }
 
-    return baseResponseDto.disposeResponse();
-  }
+    @Post('register/:user')
+    async register(
+        @Param('user') user: string,
+        @Body(new ValidationPipe()) registerUserDto: CreateUserDto,
+    ) {
+        const baseResponseDto: BaseResponseDto<User> = new BaseResponseDto();
+        const newUser = await this.localAuthFactoryService.createNewUser(
+            registerUserDto,
+        );
+        try {
 
+            switch (user) {
+                case Roles.DEALER: {
+                    const createdUser = await this.localAuthService.registerUser(newUser, Roles.DEALER);
+                    baseResponseDto.successExecWithoutType(newUser);
 
-  // for testing purpose to check routes authentication working or not
-  @Get('test')
-  @UseGuards(AuthGuard('jwt'))
-  getHello(): string {
-    return 'Authentication working successfully';
-  }
+                    break;
+                }
+                case Roles.MERCHANT: {
+                    //merchant will have seperate fields and Dto and validation logic
+                    const createdUser = await this.localAuthService.registerUser(newUser, Roles.MERCHANT);
+                    baseResponseDto.successExecWithoutType(newUser);
 
-  @Post('login')
-  async login(@Body() authenticateRequest: CreateUserDto) {
-    const baseResponseDto: BaseResponseDto<User> = new BaseResponseDto();
-    try {
-      const payload = await this.localAuthService.authenticateUser(
-        authenticateRequest,
-      );
-      baseResponseDto.successExecWithoutType(payload);
-    } catch (e) {
-      baseResponseDto.errorExec(e);
+                    break;
+                }
+                case Roles.SERVICE_PROVIDER: {
+                    //SP will have seperate fields and Dto and validation logic
+                    const createdUser = await this.localAuthService.registerUser(newUser, Roles.SERVICE_PROVIDER);
+                    baseResponseDto.successExecWithoutType(newUser);
+                    break;
+                }
+                default: {
+                    baseResponseDto.errorExec(new Error('Invalid Request'));
+                    break;
+                }
+            }
+        } catch (error) {
+            // report and log error
+            baseResponseDto.errorExec(error);
+        }
+
+        return baseResponseDto.disposeResponse();
     }
 
-    return baseResponseDto.disposeResponse();
-  }
+
+    // for testing purpose to check routes authentication working or not
+    @Get('test')
+    @UseGuards(AuthGuard('jwt'))
+    getHello(): string {
+        return 'Authentication working successfully';
+    }
+
+    @Post('login')
+    async login(@Body() authenticateRequest: CreateUserDto) {
+        const baseResponseDto: BaseResponseDto<User> = new BaseResponseDto();
+        try {
+            const payload = await this.localAuthService.authenticateUser(
+                authenticateRequest,
+            );
+            baseResponseDto.successExecWithoutType(payload);
+        } catch (e) {
+            baseResponseDto.errorExec(e);
+        }
+
+        return baseResponseDto.disposeResponse();
+    }
 }
